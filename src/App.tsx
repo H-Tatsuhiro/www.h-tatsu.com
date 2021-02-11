f613e264fdb6879d7d8e6daedbbebbd64ac55e54
H.Tatsuhiro <admin@h-tatsu.com>
2021-02-11 19:06:23 +0900

    React標準の表記を編集

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './main_logo.png';
 import './App.css';
 
 function App() {
@@ -8,16 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          H.Tatsuhiro (Tatsuhiro Hashimoto)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/H-Tatsuhiro"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
+
       </header>
     </div>
   );
